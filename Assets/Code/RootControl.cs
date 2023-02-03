f002a7c76b2d545b093534e92205bebfd83d772e
Moses Tong <moses.tong@dal.ca>
2023-02-03 19:43:30 -0400

    Added RootControl

--- Assets/Code/RootControl.cs
+++ Assets/Code/RootControl.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamF : MonoBehaviour
+public class RootControl : MonoBehaviour
 {
+    GameObject root;
     // Start is called before the first frame update
     void Start()
     {
-        
+        root = GameObject.Find("Cube");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        root.gameObject.transform.localScale += newVector3(0, -50, 0);
+
+
+
     }
 }
