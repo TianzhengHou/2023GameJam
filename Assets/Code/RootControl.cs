78aba834b68d76223f8015664e5e89b802e1a047
Austin <austinputland6464@gmail.com>
2023-02-03 20:30:12 -0400

    Added down movement to root

--- Assets/Code/RootControl.cs
+++ Assets/Code/RootControl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RootControl : MonoBehaviour
 {
+    public float distance = -0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class RootControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = transform.position + new Vector3(0.0f, distance, 0.0f);
+        }
     }
 }
