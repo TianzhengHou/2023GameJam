fed7aab0f9f3c7e9bf990a3eeaa68254529ed34a
Moses Tong <moses.tong@dal.ca>
2023-02-05 14:48:04 -0400

    Hard coding in the game bounds so that rocks don't spawn out of bounds

--- Assets/Code/RockControl.cs
+++ Assets/Code/RockControl.cs
@@ -6,12 +6,13 @@ public class RockControl : MonoBehaviour
 {
     public GameObject rock;
     public bool spowend = false;
-    private Vector2 screenBounds;
+    //private Vector2 screenBounds;
     public float respawnTime = 1.0f;
+	float screenBounds = 27f;
     // Start is called before the first frame update
     void Start()
     {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height / 2, Camera.main.transform.position.z));
+        //screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height / 2, Camera.main.transform.position.z));
         StartCoroutine(rockWave());
     }
 
@@ -19,7 +20,7 @@ public class RockControl : MonoBehaviour
     {
         GameObject gameObject = Instantiate(rock) as GameObject;
         Vector3 rootPos = GameObject.Find("Root").transform.position;
-        gameObject.transform.position = new Vector3(Random.Range(-screenBounds.x, screenBounds.x), rootPos.y - 9.5f,-1);
+        gameObject.transform.position = new Vector3(Random.Range(-screenBounds, screenBounds), rootPos.y - 9.5f,-1);
 	float scaleFactor = Random.Range(0.1f, 1f); 
 	gameObject.transform.localScale = new Vector3(scaleFactor, scaleFactor, 0);
     }
